f3c47de96ba75bbbb3db3eb2b87713a9a78b84eb
mdasif-me <cn.mdasif@gmail.com>
2025-12-10 15:49:48 +0600

    feat: add Checkbox and Dropdown Menu components with various subcomponents
    - Implemented Checkbox component with CheckboxRoot and CheckboxIndicator.
    - Added DropdownMenu component along with DropdownMenuCheckboxItem, DropdownMenuContent, DropdownMenuGroup, DropdownMenuItem, DropdownMenuLabel, DropdownMenuRadioGroup, DropdownMenuRadioItem, DropdownMenuSeparator, DropdownMenuShortcut, DropdownMenuSub, DropdownMenuSubContent, DropdownMenuSubTrigger, and DropdownMenuTrigger.
    - Created Table component with TableBody, TableCaption, TableCell, TableEmpty, TableFooter, TableHead, TableHeader, and TableRow.
    - Introduced utility function for value updating in table components.
    - Added Label component for form labeling.
    - Removed competitors.vue page.

---
+++ src/components/data-table/DragHandle.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { Button } from '@/components/ui/button';
+import { IconGripVertical } from '@tabler/icons-vue';
+import { useSortableContext } from 'dnd-kit-vue';
+
+const { handleRef } = useSortableContext();
+</script>
+
+<template>
+  <Button
+    :ref="handleRef"
+    variant="ghost"
+    size="icon"
+    class="text-muted-foreground size-7 hover:bg-transparent"
+  >
+    <IconGripVertical class="text-muted-foreground size-3" />
+    <span class="sr-only">Drag to reorder</span>
+  </Button>
+</template>
---
+++ src/components/data-table/DraggableRow.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+import { TableCell, TableRow } from '@/components/ui/table';
+import type { Row } from '@tanstack/vue-table';
+import { FlexRender } from '@tanstack/vue-table';
+import { useSortable } from 'dnd-kit-vue';
+import type { z } from 'zod';
+import type { schema } from './Index.vue';
+
+const props = defineProps<{
+  row: Row<z.infer<typeof schema>>;
+  index: number;
+}>();
+
+const { elementRef, isDragging } = useSortable({
+  id: props.row.original.id,
+  index: props.index,
+});
+</script>
+
+<template>
+  <TableRow
+    :ref="elementRef"
+    :data-state="row.getIsSelected() && 'selected'"
+    :data-dragging="isDragging"
+    class="relative z-0 data-[dragging=true]:z-10 data-[dragging=true]:opacity-80"
+  >
+    <TableCell v-for="cell in row.getVisibleCells()" :key="cell.id">
+      <FlexRender
+        :render="cell.column.columnDef.cell"
+        :props="cell.getContext()"
+      />
+    </TableCell>
+  </TableRow>
+</template>
